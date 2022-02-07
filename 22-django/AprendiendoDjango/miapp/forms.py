e906bf041f86b2af3916a577909b5b1f5171c2fc
= <=>
2022-02-07 10:34:01 +0100

    7222

--- 22-django/AprendiendoDjango/miapp/forms.py
+++ 22-django/AprendiendoDjango/miapp/forms.py
@@ -1,9 +1,24 @@
 from django import forms
+from django.core import validators
 
 class FormArticle(forms.Form):
 
     title = forms.CharField(
-        label = 'Titulo'
+        label = 'Titulo',
+        max_length=20,
+        required=True,
+        widget=forms.TextInput(
+            attrs={
+                'placeholder' : 'Mete el titulo',
+                'class' : 'titulo_form_article'
+            }
+        ),
+        
+    validators=[
+        validators.MinLengthValidator(4, 'El titulo es demasiado corto'),
+        validators.RegexValidator('^[A-Za-z0-9]*$')
+    ]
+
     )
 
     content = forms.CharField(
@@ -11,12 +26,18 @@ class FormArticle(forms.Form):
         widget=forms.Textarea
     )
 
+    content.widget.attrs.update({
+        'placeholder':'Mete el contenido aca',
+        'class' : 'contenido_form_article',
+        'id' : 'contenido_form'
+    })
+
     public_options = [
         (1, 'Si'),
         (0, 'No')
     ]
 
     public = forms.TypedChoiceField(
-        label = '¿Publicado?',
+        label = '¿Publicado?', 
         choices = public_options
     )
